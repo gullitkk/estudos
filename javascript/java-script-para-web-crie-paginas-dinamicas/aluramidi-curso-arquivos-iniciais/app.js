d8f03b5e1117790bf584e890e043d5d0201e7346
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-17 00:24:12 -0300

    1

--- javascript/java-script-para-web-crie-paginas-dinamicas/aluramidi-curso-arquivos-iniciais/app.js
+++ javascript/java-script-para-web-crie-paginas-dinamicas/aluramidi-curso-arquivos-iniciais/app.js
@@ -1,4 +1,27 @@
-const teclaPom = document.querySelector('.tecla_pom')
-function pom() {
-    alert('fui clicado');    
+function somPom() {
+    document.getElementById('som_tecla_pom').play();
+}
+function somClap() {
+    document.getElementById('som_tecla_clap').play();
+}
+function somTip() {
+    document.getElementById('som_tecla_tip').play();
+}
+function somPuff() {
+    document.getElementById('som_tecla_puff').play();
+}
+function somSplash() {
+    document.getElementById('som_tecla_splash').play();
+}
+function somToim() {
+    document.getElementById('som_tecla_toim').play();
+}
+function somPsh() {
+    document.getElementById('som_tecla_psh').play();
+}
+function somTic() {
+    document.getElementById('som_tecla_tic').play();
+}
+function somTom() {
+    document.getElementById('som_tecla_tom').play();
 }
