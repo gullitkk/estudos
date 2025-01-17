8d2ee4af1d96f80ca17b62e77552b7011bc69af1
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-17 00:51:54 -0300

    Update app.js

--- javascript/java-script-para-web-crie-paginas-dinamicas/aluramidi-curso-arquivos-iniciais/app.js
+++ javascript/java-script-para-web-crie-paginas-dinamicas/aluramidi-curso-arquivos-iniciais/app.js
@@ -2,31 +2,5 @@
 function tocarSom(id) {
     document.getElementById(id).play();
 }
-// chamando as funçãos pelo botão e acionando a função tocar som//
-function somPom() {
-   tocarSom('som_tecla_pom');
-}
-function somClap() {
-    tocarSom('som_tecla_clap');
-}
-function somTip() {
-    tocarSom('som_tecla_tip');
-}
-function somPuff() {
-    tocarSom('som_tecla_puff');
-}
-function somSplash() {
-    tocarSom('som_tecla_splash');
-}
-function somToim() {
-    tocarSom('som_tecla_toim');
-}
-function somPsh() {
-    tocarSom('som_tecla_psh');
-}
-function somTic() {
-    tocarSom('som_tecla_tic');
-}
-function somTom() {
-    tocarSom('som_tecla_tom');
-}
+// criei uma const e igualei ela a tudo q tiver a class tecla ou seja todas as teclas//
+const teclasSoms = document.querySelectorAll('tecla'); 
