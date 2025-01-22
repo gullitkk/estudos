b9811b80d867884f4fb213c4a718bbd62e384c05
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-22 20:36:55 -0300

    1

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Banner/Banner.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Banner/Banner.jsx
@@ -1,6 +1,11 @@
+import './Banner.css'
+
 function Banner() {
     return (
-        <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        <header className = 'banner'>
+             <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header> 
+        
         // parece html mas na verdade e um jsx, jsx nada mais e que um tradutor ele pega esse codigo que eu escrevi aparatemente html e transforma em um codigo que serviria pro js, por exemplo nesse caso o codigo seria tipo um document.create('img')// 
 
         //o alt e importante na hora de colocar uma imagem pois e um texto para exemplificar oque e aquela imagem, esse texto nao aparece// 
