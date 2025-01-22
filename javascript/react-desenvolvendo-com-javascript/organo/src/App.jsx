0ad911e432abea4143bd53ab1917050d14f59a20
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-22 15:02:24 -0300

    1

--- javascript/react-desenvolvendo-com-javascript/organo/src/App.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Banner from './componetes/Banner/Banner';
+import Banner from './componetes/Banner/banner'
+
 
 
 function App() {
