a5dbec99d4a05cefcd086485a3c5bb5d82f3fcd5
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-22 20:26:06 -0300

    1

--- javascript/react-desenvolvendo-com-javascript/organo/src/App.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Banner from './componetes/Banner/Banner.js';
+import Banner from './components/Banner/Banner';
 
 
 
@@ -12,7 +12,7 @@ function App() {
   // o react e isso a gente cria componetes e importa pro principal // 
   return (
     <>  
-      < Banner />
+      <Banner />
       <div>
       
           <a href="https://vite.dev" target="_blank">
--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Banner/Banner.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Banner/Banner.jsx
@@ -7,4 +7,5 @@ function Banner() {
     )
 
 }
+
 export default Banner
