e8bd9a130ec56e622990727306df2449ee777261
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-23 20:49:01 -0300

    1

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/CampoTexto/CampoTexto.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/CampoTexto/CampoTexto.jsx
@@ -1,5 +1,6 @@
 import './CampoTexto.css'
-const CampoTexto = () => {
+
+export const CampoTexto = () => {
     return (
         <div className="form_criar_card"> 
             <h2>Preencha os dados para criar o card do colaborador.</h2>
@@ -9,11 +10,13 @@ const CampoTexto = () => {
             <input type="text" placeholder="Digite seu cargo"/>
             <label>Imagem</label>
             <input type="text" placeholder="Informe o endereço da imagem" />
+            <label>Time</label>
+            <select name="" id=""></select>
+            <button placeholder='Criar card' id='buttun_criar_card'></button>
         </div>
     )
 }
-
-export default CampoTexto 
+//aqui colocamos o export direto na function// 
 //aqui a gente n usa o class para atribuir classes, usamos className // 
 //essa estrutura faz a mesma coisa que a estrutura do banner, a diferença e que com arrow function o codigo fica menos verbalizado//  
 //aqui criei uma cost chamada CampoTexto igualei ela a arrow function, o parantese e oque declara a função, seria equivalente a escrever function, caso queira colocar parametros coloque dentro dele, e a flecha e mais estrutura msm, as chaves e estrutura tbm// 
---
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/CampoTexto/index.jsx
@@ -0,0 +1,6 @@
+import { CampoTexto } from "./CampoTexto";
+
+export default CampoTexto
+//como colocamos o export direto na function temos que especificar oque estamos importando colocando uma chaves naquilo que a gente importou//
+//depois damos um export default padrão//
+//la no app.jsx ele identifica o index.jsx e ja importa// 
