1fda725a05e213a1e05b76b2293f438843634642
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-23 20:09:04 -0300

    Update CampoTexto.jsx

---
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/CampoTexto/CampoTexto.jsx
@@ -0,0 +1,9 @@
+const CampoTexto = () => {
+    return (
+        
+    )
+}
+
+export default CampoTexto 
+//essa estrutura faz a mesma coisa que a estrutura do banner, a diferença e que com arrow function o codigo fica menos verbalizado//  
+//aqui criei uma cost chamada CampoTexto igualei ela a arrow function, o parantese e oque declara a função, seria equivalente a escrever function, caso queira colocar parametros coloque dentro dele, e a flecha e mais estrutura msm, as chaves e estrutura tbm// 
