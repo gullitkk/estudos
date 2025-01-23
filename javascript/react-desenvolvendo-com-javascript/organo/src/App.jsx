80f15e6b836ccc2377364c0a939829945fdb2453
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-23 14:09:30 -0300

    1

--- javascript/react-desenvolvendo-com-javascript/organo/src/App.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/App.jsx
@@ -7,6 +7,7 @@ import Banner from './components/Banner/Banner';
 
 
 function App() {
+  
   const [count, setCount] = useState(0)
   // chamamos o banner pra dentro da function ou seja para ele aparecer, fechamos ele com o /> e o vs code automaticamente fez um import la em cima desse componete que a gente criou//
   // o react e isso a gente cria componetes e importa pro principal // 
