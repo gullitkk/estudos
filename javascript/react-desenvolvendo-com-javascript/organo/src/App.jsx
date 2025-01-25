6538e1b53fa531b4ed9a0dcc433c36a044841c23
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-25 00:23:21 -0300

    Update App.jsx

--- javascript/react-desenvolvendo-com-javascript/organo/src/App.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
 import Banner from './components/Banner'; //colocando o nome de index.jsx voce so precisa colocar o nome da pasta no import pois o import ja identifica o jsx//
+import Formulario from './components/Formulario';
 
 function App() {
-  
-  const [count, setCount] = useState(0)
   // chamamos o banner pra dentro da function ou seja para ele aparecer, fechamos ele com o /> e o vs code automaticamente fez um import la em cima desse componete que a gente criou//
   // o react e isso a gente cria componetes e importa pro principal // 
+  // tiramos as coisas do campo texto daqui pois vamos iplementar ele no formulario, como ele vai ficar dentro do formulario e mais facil de  importar ele la, e depois so exporta o formulario aqui//
   return (
     <>  
       <Banner />
+      <Formulario />
     </>
   ) 
 }
