1e9110be5063e53429f583b30f97152aa06b72df
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-26 23:23:02 -0300

    1

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Formulario/Formulario.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Formulario/Formulario.jsx
@@ -1,13 +1,23 @@
 import CampoTexto from '../CampoTexto' //criamos um arquivo chamado index.jsx e la exportamos e importamos oque a gente quiser, esse metodo e parecido com o do banner de so renomear o arquivo para index.jsx mas aqui o nosso arquivo principal continua com o nome desejado e a gente escolhe oque quer importar, a partir de agora vou usar esse jeito// 
+import { ListaSuspensa } from '../ListaSuspensa/ListaSuspensa'
 import './Formulario.css'
 
 export const Formulario = () => {
+
+    const itens = [
+        'Top lane',
+        'Mid lane',
+        'bot lane',
+        'jg'
+    ]
+
     return (
         <section className='formulario'>
             <form>
             <CampoTexto label='Nome do campeão' placeholder='Digite o nome do seu campão'/>  
             <CampoTexto label='Função'  placeholder='Digite a função do seu campão'/>
             <CampoTexto label='imagem'  placeholder='Informe o endereço da imagem'/>
+            <ListaSuspensa label='posição' itens={itens}/>
             </form>
         </section> // aqui criamos parametros, agora ele esta mandando essas informaçãos que colocamos entre aspas la pra função campo texto// 
     )
