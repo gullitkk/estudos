c068c7b4dcbfde7b69c93c7d4b926b2342981930
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-28 22:24:35 -0300

    Update CampoTexto.jsx

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/CampoTexto/CampoTexto.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/CampoTexto/CampoTexto.jsx
@@ -2,8 +2,11 @@ import './CampoTexto.css'
 
 export const CampoTexto = (props) => { // aqui criamos um parametro para receber aqueles que estamos mandando no app.jsx, o nome n importa mas na maioria das vezes usamos props// 
 
-    const aoValor = () => {
-        console.log(aoValor)
+    let valor = ''
+
+    const aoValor = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
     }
 
     return ( // aqui colocamos props.label porque estamos chamano o parametro e especificando o caminho label, tem que colocal entre chaves se não ele entende como texto, o mesmo vale para o props.placeholder. // 
