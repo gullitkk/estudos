e5dd543ddc167c51331d246faf65a42b15044e97
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-29 00:02:26 -0300

    Update Formulario.jsx

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Formulario/Formulario.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Formulario/Formulario.jsx
@@ -21,7 +21,7 @@ export const Formulario = () => {
 
     const aoSalvar = (evento) => { // esse parametro evento e do js, o evento do onsubmit que seria o acionamento do botão// 
         evento.preventDefault() //essa função ela remove o comportamento padrão, porque o comportamento padrão seria recarregar a pagina mas a gnt n quer isso//
-        console.log('O form foi submetido =>', nome, imagem, função)
+        console.log('O form foi submetido =>', nome, imagem, função, posição)
     }
 
     return (
