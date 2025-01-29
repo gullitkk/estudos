1407a373e73b72a7f328b1e83a819775d3e9cc1d
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-28 23:14:26 -0300

    1

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Formulario/Formulario.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Formulario/Formulario.jsx
@@ -29,7 +29,7 @@ export const Formulario = () => {
                 label='Nome do campeão'
                 placeholder='Digite o nome do seu campão'/>  
                 valor = {nome}
-                aoAlterado
+                aoAlterado = {valor => setNome(valor)} 
             <CampoTexto 
                 obrigatorio={true} 
                 label='Função' 
@@ -41,6 +41,7 @@ export const Formulario = () => {
             </Botao>
             </form> 
         </section> 
+        //definimos em cada um que o valor vai ser predefinido, e o aoAlterado que seria os novos valores vai ser igual a valor function ai setNome que a lista que altera(valor)
         //ali nos 2 primeiros e ultimo campotexto eu coloquei que obrigatorio um parametro qualquer e igual ao valor true, e la no campotexto.jsx e listasuspensa.jsx eu falei que required e igual a esse parametro,ou seja de forma simplificada required = true tornando required on de forma que ele junte os mundos html5 com o react, isso para os campos que eu botei obrigatorio serem obrigatorios a ser prenchidos// 
         //onSubmit seria quando o formulario for enviado seja por um botão ou tecla, falamamos que quando o formulario foi enviado ele vai acionar a função aoSalvar// 
         // aqui colocamos o texto do corpo do botao, fazendo o texto se tornar filho do botão// 
