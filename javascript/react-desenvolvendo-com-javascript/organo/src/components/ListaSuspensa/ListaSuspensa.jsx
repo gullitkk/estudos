b93e5e11f1518bd205f32fc7a086b3f68ba1b903
heitor <155327672+gullitkk@users.noreply.github.com>
2025-01-28 23:58:27 -0300

    Update ListaSuspensa.jsx

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/ListaSuspensa/ListaSuspensa.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/ListaSuspensa/ListaSuspensa.jsx
@@ -4,7 +4,7 @@ export const ListaSuspensa = (props) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select required={props.obrigatorio}> 
+            <select required={props.obrigatorio} value={props.valor} onChange={evento => props.aoAlterado(evento.target.value)}> 
                 {props.itens.map(itens => {
                     return <option key={itens}>{itens}</option>
                 }) }
