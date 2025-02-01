59679364177ade71eb52f61fe340b9e874bd8173
heitor <155327672+gullitkk@users.noreply.github.com>
2025-02-01 01:01:45 -0300

    1

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Colaborador/Colaborador.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Colaborador/Colaborador.jsx
@@ -3,10 +3,10 @@ import './Colaborador.css'
 export const Colaborador = () => {
     return (
         <div>
-            <img src="https://github.com/" alt=""/>
+            <img src="https://github.com/gullitkk.png" alt="heitor"/>
             <div>
-                <h4>a</h4>
-                <h5></h5>
+                <h4>Heitor</h4>
+                <h5>aluno</h5>
             </div>
         </div>
     )
