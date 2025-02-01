bf94232cad7559049d088d155de0019360bdcc34
heitor <155327672+gullitkk@users.noreply.github.com>
2025-02-01 01:01:00 -0300

    1

---
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Colaborador/Colaborador.jsx
@@ -0,0 +1,13 @@
+import './Colaborador.css'
+
+export const Colaborador = () => {
+    return (
+        <div>
+            <img src="https://github.com/" alt=""/>
+            <div>
+                <h4>a</h4>
+                <h5></h5>
+            </div>
+        </div>
+    )
+}
