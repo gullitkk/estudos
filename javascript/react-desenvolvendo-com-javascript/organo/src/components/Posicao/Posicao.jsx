35cb723d3391bfbe078a558f6c6236d5fb95a536
heitor <155327672+gullitkk@users.noreply.github.com>
2025-02-01 00:19:36 -0300

    Update Posicao.jsx

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Posicao/Posicao.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Posicao/Posicao.jsx
@@ -2,7 +2,7 @@ import './Posicao.css'
 
 export const Posicao = (props) => {
     return (
-        <section className='Time' style={colo}>
+        <section className='Time' style={{backgroundColor: props.corSecundaria}}>
             <h3>{props.nome}</h3>
         </section>
     )
