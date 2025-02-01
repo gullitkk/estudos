c0ddc1e2b765976fe3fd23a289330661fff911a8
heitor <155327672+gullitkk@users.noreply.github.com>
2025-02-01 00:17:37 -0300

    1

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Posicao/Posicao.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Posicao/Posicao.jsx
@@ -2,8 +2,7 @@ import './Posicao.css'
 
 export const Posicao = (props) => {
     return (
-        
-        <section className='Time'>
+        <section className='Time' style={colo}>
             <h3>{props.nome}</h3>
         </section>
     )
