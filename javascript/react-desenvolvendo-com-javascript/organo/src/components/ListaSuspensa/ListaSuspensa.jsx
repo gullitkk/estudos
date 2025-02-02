818dfe4c2b59b048e8760c9f43c7fb39509d7b82
heitor <155327672+gullitkk@users.noreply.github.com>
2025-02-02 19:07:09 -0300

    Update ListaSuspensa.jsx

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/ListaSuspensa/ListaSuspensa.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/ListaSuspensa/ListaSuspensa.jsx
@@ -5,6 +5,7 @@ export const ListaSuspensa = (props) => {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select required={props.obrigatorio} value={props.valor} onChange={evento => props.aoAlterado(evento.target.value)}> 
+                <option value=""></option>
                 {props.itens.map(itens => {
                     return <option key={itens}>{itens}</option>
                 }) }
@@ -12,6 +13,7 @@ export const ListaSuspensa = (props) => {
         </div>
     )
 }
+//o option com nada dentro e value = '', e pra ter uma opção vazia no selectt// 
 //o lista suspensa e uma lista generica, podemos usar para os times ou qualquer outra lista//
 //o select e aquele campo de escolher dentro dele colocamos o props.itens que e um parametro que ta vindo do formulario.jsx e botamos o .map que e um recurso do react para listas, ele percorre a lista executa ela e criar novas opçãos a partir dela//
 //criamos uma arrow function com o parametro itens, o parantese ebranje tudo para ter menos codigo, mas no fim e a mesma coisa, e essa function quando acionada retorna um valor do option, que seria o itens, a key e algo nessecaria do react, igualamos ela a itens pois cada time tem seu nome ou seja a key e o nome//
