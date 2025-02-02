d9c519f91de94c92ec51c39e5f4569a17c0d947e
heitor <155327672+gullitkk@users.noreply.github.com>
2025-02-02 16:30:08 -0300

    Update Posicao.jsx

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Posicao/Posicao.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Posicao/Posicao.jsx
@@ -5,7 +5,7 @@ export const Posicao = (props) => {
     return (
         <section className='Time' style={{ backgroundColor: props.corSecundaria }}> 
             <h3 style={{ borderColor: props.corPrimaria}}>{props.nome}</h3>
-            {props.campeaos.map( campeaos => <Campeao nome={campeaos.nome} posicao={campeaos.posicao} imagem={campeaos.imagem} /> )}
+            {props.campeaos.map( campeao => <Campeao nome={campeao.nome} posicao={campeao.posicao} imagem={campeao.imagem} /> )}
         </section>
     ) // precisamos alterar a cor do fundo e da letra usando props como o css n aceita props, usamos o css inlinem dentro do lugar que vc quer aplicar abr um style dentro do style abra uma segunda chaves ai altere// 
 }
