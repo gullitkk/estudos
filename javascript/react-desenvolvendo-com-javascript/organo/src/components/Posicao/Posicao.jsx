93a522186aa193171f0da0fb21327c5b50d08522
heitor <155327672+gullitkk@users.noreply.github.com>
2025-02-02 18:39:19 -0300

    Update Posicao.jsx

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Posicao/Posicao.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Posicao/Posicao.jsx
@@ -3,12 +3,13 @@ import Campeao from '../Campeao'
 
 export const Posicao = (props) => {
     return (
+        props.campeao.length > 0 &&
         <section className='Time' style={{ backgroundColor: props.corSecundaria }}> 
             <h3 style={{ borderColor: props.corPrimaria}}>{props.nome}</h3>
             <section className='campeao'>
                 {props.campeao.map( campeao => <Campeao nome={campeao.nome}  função={campeao.função} imagem={campeao.imagem}/> )}   
             </section>
-            
         </section>
+        //aquele props.campeao.length > 0 && seria se a quantidade de campeao ou seja da lista vou maior que zero ele retorna a função que aciona o campo bot lane, etc. se n ele n aciona, posso usar esse metodo ou o da virgula// 
     ) // precisamos alterar a cor do fundo e da letra usando props como o css n aceita props, usamos o css inlinem dentro do lugar que vc quer aplicar abr um style dentro do style abra uma segunda chaves ai altere// 
 }
