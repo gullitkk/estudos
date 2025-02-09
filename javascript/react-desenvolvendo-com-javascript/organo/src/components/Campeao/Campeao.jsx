cfa08ecff94d4c370b94fd0fb29313c8b3776b5d
heitor <155327672+gullitkk@users.noreply.github.com>
2025-02-09 00:50:00 -0300

    1

--- javascript/react-desenvolvendo-com-javascript/organo/src/components/Campeao/Campeao.jsx
+++ javascript/react-desenvolvendo-com-javascript/organo/src/components/Campeao/Campeao.jsx
@@ -1,9 +1,10 @@
+import { IoMdCloseCircle } from "react-icons/io";
 import './Campeao.css'
 
 export const Campeao = (props) => {
     return (
     <div className='colaborador'>
-        <div className='deletar' onClick={props.aoDeletar}>Deletar</div>
+       <IoMdCloseCircle 'deletar' onClick={props.aoDeletar} /> 
         <div className='cabecalho' style={{backgroundColor: props.corFundo}}>
             <img src={props.imagem}alt={props.nome}/>
         </div>
